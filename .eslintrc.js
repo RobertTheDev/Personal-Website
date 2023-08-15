fc32b628eee5cdc68a699501298d7a67237796d7
RobertTheDev <robertthedev@gmail.com>
2023-08-15 14:49:01 +0100

    feat: installed and formatted prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,17 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
+<<<<<<< HEAD
   extends: [`react-app`, 'prettier'],
+=======
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'react-app', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+>>>>>>> ece9227 (feat: installed and formatted prettier)
 };
